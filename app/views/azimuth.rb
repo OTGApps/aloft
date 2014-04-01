4c5a85df69754d4c89ac6995d36383bb6dccea30
Mark Rickert <mjar81@gmail.com>
2014-04-01 11:14:26 -0400

    Turning on and off bearing updates works properly now.

--- app/views/azimuth.rb
+++ app/views/azimuth.rb
@@ -1,17 +1,20 @@
 class Azimuth < UIImageView
   def observe_location
-    App.notification_center.observe 'HeadingUpdate' do |notification|
-      @phone_heading = notification.object
-      animate_to_bearing(0)
-    end
+    @observing ||= begin
+      App.notification_center.observe 'HeadingUpdate' do |notification|
+        @phone_heading = notification.object
+        animate_to_bearing
+      end
 
-    App.notification_center.observe 'StopHeadingUpdates' do |notification|
-      puts 'Stopping heading updates.'
-      spring_to_bearing
+      App.notification_center.observe 'StopHeadingUpdates' do |notification|
+        ap 'Stopping heading updates.' if BW.debug?
+        animate_to_bearing
+      end
     end
   end
 
   def bearing=(b)
+    @original_bearing ||= b
     @bearing = b
     animate_to_bearing
   end
@@ -20,8 +23,13 @@ class Azimuth < UIImageView
     (@bearing || 0) - (@phone_heading || 0)
   end
 
-  def animate_to_bearing(timing = 2.0)
-    UIView.animateWithDuration(timing,
+  def animate_to_bearing
+    if App::Persistence['compass'] == false
+      ap "Animating - compass is off" if BW.debug?
+      return spring_to_original_bearing
+    end
+
+    UIView.animateWithDuration(0.0,
       delay:0.0,
       options:UIViewAnimationOptionCurveLinear,
       animations: lambda {
@@ -34,14 +42,14 @@ class Azimuth < UIImageView
   end
 
   # This animation is used only when the user doesn't want compass updates.
-  def spring_to_bearing(timing = 2.0)
+  def spring_to_original_bearing(timing = 2.0)
     UIView.animateWithDuration(timing,
       delay:0.3,
       usingSpringWithDamping:0.3,
       initialSpringVelocity:0.2,
       options:UIViewAnimationOptionCurveLinear,
       animations: lambda {
-        radians = CGAffineTransformMakeRotation(combined_bearing.to_i * Math::PI / 180);
+        radians = CGAffineTransformMakeRotation(@original_bearing.to_i * Math::PI / 180);
         self.transform = radians
       },
       completion:lambda {|finished|
